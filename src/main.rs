3ca369d2369872f362795d65cb03940f27cbeb2a
Krzysztof Majerczyk-Marchewka <skanerooo5@gmail.com>
2023-12-01 13:31:33 +0100

    Day 1 solution. For now everything will be in one file, because I'm too lazy

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,7 @@
-use actix_web::{get, web::ServiceConfig, HttpResponse};
+use actix_web::{get, web::ServiceConfig, HttpResponse, web};
 use shuttle_actix_web::ShuttleActixWeb;
+
+// day -1
 
 #[get("/")]
 async fn hello_world() -> &'static str {
@@ -11,12 +13,28 @@ async fn fake_error() -> HttpResponse {
     HttpResponse::InternalServerError().finish()
 }
 
+// day 1
+
+#[get("/1/{nums}*")]
+async fn cube_the_bits(path: web::Path<String>) -> String {
+    let args = path.into_inner().as_str().rsplit('/')
+                           .map(|s| s.parse::<isize>().expect("failed to parse number"))
+                           .collect::<Vec<isize>>();
+    let mut xored = 0;
+    for arg in args {
+        xored ^= arg;
+    }
+    let cube = xored.pow(3);
+    format!("{cube}")
+}
+
 #[shuttle_runtime::main]
 #[allow(clippy::unused_async)]
 async fn main() -> ShuttleActixWeb<impl FnOnce(&mut ServiceConfig) + Send + Clone + 'static> {
     let config = move |cfg: &mut ServiceConfig| {
         cfg.service(hello_world);
         cfg.service(fake_error);
+        cfg.service(cube_the_bits);
     };
 
     Ok(config.into())
